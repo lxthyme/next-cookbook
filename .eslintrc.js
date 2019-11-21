e2147a3d148eea8de235748717058592c65cb197
lxthyme <1094426094@qq.com>
2019-11-21 23:05:26 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   ],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+    'maxLen': 80,
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # next-cookbook
 next.js cookbook
+
+
+
+
+
+#### Animation
+
+[react-spring](https://www.react-spring.io)
+
+[react-motion](https://github.com/chenglou/react-motion)
