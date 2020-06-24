b50fe34b206ffe56e0769f5c8fa4da37dd83639a
lxthyme <1094426094@qq.com>
2020-06-24 17:36:30 +0800

    prj config

--- next.config.js
+++ next.config.js
@@ -1,143 +1,155 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+/* eslint-disable */
+// module.exports = require('./next-config/css-in-js.config').withLess
+// module.exports = require('./next-config/less.config')
+/* eslint-disable */
 
-const isProd = process.env.NODE_ENV === 'production'
+const withLess = require('@zeit/next-less')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
 
-// const cssInJS = require('./next-config/css-in-js.config')
-// module.exports = cssInJS.withSass
-// module.exports = cssInJS.withCSS
+// development or other environment
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+
+/****************************** Antd Less Config ******************************/
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(
+    path.resolve(__dirname, './public/ant/ant.custom.less'),
+    'utf8'
+  )
+)
+
+// // fix antd bug in dev development
+// const devAntd = '@import "~antd/dist/antd.less";\n';
+// const stylesData = fs.readFileSync(
+//   path.resolve(__dirname, './public/less/_styles.less'),
+//   'utf-8'
+// );
+// fs.writeFileSync(
+//   path.resolve(__dirname, './public/less/self-styles.less'),
+//   isDev ? `${devAntd}${stylesData}` : stylesData,
+//   'utf-8'
+// );
+
+// // fix: prevents error when .css files are required by node
+// if (typeof require !== 'undefined') {
+//   require.extensions['.less'] = () => {}
+// }
+
+/**
+ * 1. @zeit/next-less
+ * 2. less
+ * 3. less-vars-to-js
+ */
+const withLessConfig = {
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables, // make your antd custom effective
+  },
+  webpack: (config, { isServer }) => {
+    const rules = config.module.rules
+    // console.log(`>>>[${rules.length}]: `, rules)
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
+    }
+    return config
+  },
+}
 
 // const bundleAnalyzer = require('./next-config/bundle-analyzer')
 // module.exports = bundleAnalyzer.defaultBundleAnalyzer
 
-const env = require('./next-config/env.config')
-module.exports = env.ENV
+const withCss = require('@zeit/next-css')
+const withCssConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
 
-console.log({
-  SECRET: process.env.SECRET,
-  ANOTHER_SECRET: process.env.ANOTHER_SECRET
-})
-
-// const cdn = require('./next-config/cdn.config')
-// module.exports = cdn.CDN
-
-module.exports = (phase, { defaultConfig }) => {
-  //   console.log("-->phase: ", phase);
-  //   console.log("-->defaultConfig: ", defaultConfig);
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      /* development only config options here */
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
     }
-  }
-  return {
-    /* config options for all phases except development here */
-    // target: "serverless",
-    // target: "server",
-    // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://cdn.mydomain.com' : '',
-    poweredByHeader: false,
-    // pageExtensions: ['mdx', 'jsx', 'js'],
-    env: {
-      SECRET: process.env.SECRET,
-      customKey: 'my-value'
-    },
-    devIndicators: {
-      autoPrerender: false
-    }
-  }
-}
-
-// module.exports = {
-//   generateBuildId: async () => {
-//     // You can, for example, get the latest git commit hash here
-//     return "my-build-id";
-//   }
-// };
-
-/// Webpack config
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-
-    // Fixes npm packages that depend on `fs` module
-    // config.node = {
-    //   fs: 'empty'
-    // }
     return config
   },
-  webpackDevMiddleware: config => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
-    return config
-  }
 }
 
-module.exports = {
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    console.log('defaultPathMap: ', defaultPathMap)
-    return {
-      '/a': { page: '/nextjs-doc/9.api-reference/3.next-link' },
-      '/b': { page: '/nextjs/about' }
-    }
-  }
-}
+const withPlugins = require('next-compose-plugins')
+module.exports = withPlugins(
+  [
+    [withLess, withLessConfig],
+    // [withCss, withCssConfig],
+    [withCss, {}],
+  ],
+  {}
+)
 
-// module.exports = {
-//   publicRuntimeConfig: {
-//     API_TEST: process.env.API_URL,
-//     API_BETA: 'https://apibeta.vaffle.com'
-//   }
+// const withLessExcludeAntd = require("./next-config/less2.config")
+// // choose your own modifyVars
+// const modifyVars = require("./public/ant/ant.custom.less")
+
+// if (typeof require !== 'undefined') {
+//   require.extensions['.less'] = (file) => {}
 // }
 
-/**
-const ctx = {
-    defaultConfig: {
-        env: [],
-        webpack: null,
-        webpackDevMiddleware: null,
-        distDir: '.next',
-        assetPrefix: '',
-        configOrigin: 'default',
-        useFileSystemPublicRoutes: true,
-        generateBuildId: [Function: generateBuildId],
-        generateEtags: true,
-        pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-        target: 'server',
-        poweredByHeader: true,
-        compress: true,
-        devIndicators: {
-            buildActivity: true,
-            autoPrerender: true
-        },
-        onDemandEntries: {
-            maxInactiveAge: 60000,
-            pagesBufferLength: 2
-        },
-        amp: {
-            canonicalBase: ''
-        },
-        exportTrailingSlash: false,
-        experimental: {
-            ampBindInitData: false,
-            cpus: 3,
-            css: false,
-            documentMiddleware: false,
-            granularChunks: false,
-            modern: false,
-            profiling: false,
-            publicDirectory: false,
-            sprFlushToDisk: true
-        },
-        future: {
-            excludeDefaultMomentLocales: false
-        },
-        serverRuntimeConfig: {},
-        publicRuntimeConfig: {}
-    }
-}
- */
+// module.exports = withLessExcludeAntd({
+//     cssModules: true,
+//     cssLoaderOptions: {
+//       importLoaders: 1,
+//       localIdentName: "[local]___[hash:base64:5]",
+//     },
+//     lessLoaderOptions: {
+//       javascriptEnabled: true,
+//       modifyVars: modifyVars
+//     }
+// })
+
+// const antdLessLoader = require('next-antd-aza-less')
+// const modifyVars = require('./public/ant/ant.custom.less')
+
+// if (typeof require !== 'undefined') {
+//   require.extensions['.less'] = (file) => {}
+// }
+
+// /* Without CSS Modules, with PostCSS */
+// module.exports = antdLessLoader({
+//   cssModules: true,
+//   cssLoaderOptions: {
+//     importLoaders: 1,
+//     localIdentName: '[local]___[hash:base64:5]',
+//   },
+//   lessLoaderOptions: {
+//     javascriptEnabled: true,
+//     modifyVars: modifyVars,
+//   },
+// })
