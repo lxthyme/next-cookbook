b182afe246c13eccdf868ea8924f100aa508b45d
lxthyme <lx314333@gmail.com>
2021-09-23 18:41:19 +0800

    fix(dsp): fixed

--- data/dsp/seed0-19999999_v1.js
+++ data/dsp/seed0-19999999_v1.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33340cd218b9bb455c18d41a6bd218042ef8d40aec1d774d2b477882a08ef9f4
-size 101907
+oid sha256:b34c080396c24e9a4fd170496ef7d6e3145264299dbf1b4ad6698a20dc1ed296
+size 101910
--- data/dsp/seed20000000-49999999_2.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b5b9a5db57938ce3288daa2f35bbdaac1412f54e81a001abcfd0c89f8a49765c
-size 620
