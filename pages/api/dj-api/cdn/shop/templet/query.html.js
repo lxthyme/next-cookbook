6db49b722a9c4de30c12634141c719e1f8f3eeac
lxthyme <lx314333@gmail.com>
2022-01-07 18:47:52 +0800

    feat: update

--- pages/api/dj-api/cdn/shop/templet/query.html.js
+++ pages/api/dj-api/cdn/shop/templet/query.html.js
@@ -1,37 +1,59 @@
 import {
-  mockData_Prd_dj_tab4,
-  mockData_Prd_ydj_tab4,
-  mockData_Test_dj_tab4
+	mockData_Prd_dj_tab4,
+	mockData_Prd_ydj_tab4,
+	mockData_Test_dj_tab4
 } from "../../../../../../mockData/dj/query.html";
 
 const API = async (req, res) => {
-  // const mockData = await mockGet('query.html.js', 'mockData_OnlyO2O')
-  // const obj = mockData.mockData_Prd_dj_tab4
-  // const obj = mockData.mockData_Prd_ydj_tab4
-  let obj =
-    // mockData_Prd_dj_tab4
-    mockData_Prd_ydj_tab4
-    // mockData_Test_dj_tab4
-  obj.obj.templet.moduleList[1].busiType = 4
-  // obj.obj.templet.moduleList[1].tabItemList = obj.obj.templet.moduleList[1].tabItemList.slice(0, 1)
-  obj.obj.templet.moduleList[1].tabItemList = [
-    obj.obj.templet.moduleList[1].tabItemList[0],
-    obj.obj.templet.moduleList[1].tabItemList[2],
-  ]
-  // obj.obj.templet.moduleList[1].tabItemList.forEach(t => {
-  //   t.labelTxt = '23123213'
-  //   t.showLabel = 1
-  // })
-  // obj.obj.templet.moduleList[1].labelColor = '#000000'
+	// const mockData = await mockGet('query.html.js', 'mockData_OnlyO2O')
+	// const obj = mockData.mockData_Prd_dj_tab4
+	// const obj = mockData.mockData_Prd_ydj_tab4
+	// let obj =
+	//   // mockData_Prd_dj_tab4
+	//   // mockData_Prd_ydj_tab4
+	//   mockData_Test_dj_tab4
+	// obj.obj.templet.moduleList[1].busiType = 4
+	// // obj.obj.templet.moduleList[1].tabItemList = obj.obj.templet.moduleList[1].tabItemList.slice(0, 1)
+	// obj.obj.templet.moduleList[1].tabItemList = [
+	//   obj.obj.templet.moduleList[1].tabItemList[0],
+	//   obj.obj.templet.moduleList[1].tabItemList[2],
+	// ]
+	// // obj.obj.templet.moduleList[1].tabItemList.forEach(t => {
+	// //   t.labelTxt = '23123213'
+	// //   t.showLabel = 1
+	// // })
+	// // obj.obj.templet.moduleList[1].labelColor = '#000000'
 
-  obj = mockData
-  obj.obj.templet.moduleList[1].tabItemList[0].moduleList = obj.obj.templet.moduleList[1].tabItemList[0].moduleList.slice(0, 12)
-  return new Promise(function (resolve) {
-    setTimeout(resolve.bind(null, resolve), 100)
-  })
-    .then(() => {
-      res.status(200).json(obj)
-    })
+	// obj = mockData
+	// obj.obj.templet.moduleList[1].tabItemList[0].moduleList = obj.obj.templet.moduleList[1].tabItemList[0].moduleList.slice(0, 12)
+	const { obj: tmp, ...f_objOthers } = mockData_Test_dj_tab4
+	const { tabBar, moduleList, config, ...f_templetOthers } = tmp.templet
+	const { tabBarInfoList, ...f_tabBarOthers } = tabBar
+	const obj = {
+		...f_objOthers,
+		obj: {
+			onOff: tmp.onOff,
+			templet: {
+				tabBar: {
+					tabBarInfoList,
+					...f_tabBarOthers,
+					bkPic: "https://Img.st.iblimg.com/fast2home-2/images/kdj/index/2019/09/2136514702.png",
+					barType: 2,
+					bkPicWidth: 750,
+					bkPicHeight: 98,
+				},
+				moduleList,
+				config,
+				...f_templetOthers
+			}
+		}
+	}
+	return new Promise(function (resolve) {
+		setTimeout(resolve.bind(null, resolve), 100)
+	})
+		.then(() => {
+			res.status(200).json(obj)
+		})
 }
 
 export default API
