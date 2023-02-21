5c620ba32e377d025704303cbf5cbff448969d24
lxthyme <lx314333@gmail.com>
2023-02-21 19:07:48 +0800

    feat: update

---
+++ mockData/dj/getInvoiceStatus.html.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3685e3ccae62e8338d2f6e4956f00cdebed87049075eababdbc0d25ed590be1d
+size 608
---
+++ pages/api/dj-api/kdj/invoice/getInvoiceStatus.html.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+
+  const data = mockData
+
+  return new Promise(function (resolve) {
+    setTimeout(resolve.bind(null, resolve), 1000)
+  })
+    .then(() => {
+      res.status(200).json(data)
+    })
+}
+
+const mockData = {}
