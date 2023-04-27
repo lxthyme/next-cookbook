684e964eb4bab967f789325d537d9b18d0fed055
lxthyme <lx314333@gmail.com>
2023-04-27 19:05:07 +0800

    feat: update

--- pages/api/dj-api/kdj/order/advancePay.html.ts
+++ pages/api/dj-api/kdj/order/advancePay.html.ts
@@ -4,11 +4,13 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 export default (req: NextApiRequest, res: NextApiResponse) => {
 
   const data = mockDJCart
-                    .t00011支付风控
+                    .t00100000
+                    // .t00011支付风控
                     // .t00012支付风控验证码错误
                     // .t00014未设置过密码
                     // .t00013校验密码
                     // .t00015校验支付密码错误
+                    // .t00015校验支付密码错误被锁定
                     // .t00990001优惠券风控
 
   return new Promise(function (resolve) {
