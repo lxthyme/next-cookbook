8b36c5219d42c3d1beebd0e09cde79a1a9d64830
lxthyme <lx314333@gmail.com>
2024-10-24 19:28:58 +0800

    feat: apple-app-site-association config

--- next.config.js
+++ next.config.js
@@ -32,7 +32,16 @@ module.exports = (phase, { defaultConfig }) => {
         {
           source: '/api/echarts',
           destination: 'https://cdn.jsdelivr.net',
-        },
+        }, {
+          destination: '/api/apple-app-site-association',
+          source: "/apple-app-site-association",
+          // headers: [
+          //   {
+          //     key: "Content-Type",
+          //     value: "application/json"
+          //   }
+          // ]
+        }
       ]
     },
     async headers() {
@@ -44,6 +53,15 @@ module.exports = (phase, { defaultConfig }) => {
             { key: "Access-Control-Allow-Origin", value: "*" },
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }, {
+          source: "/apple-app-site-association",
+          headers: [
+            // { key: "Access-Control-Allow-Credentials", value: "true" },
+            // { key: "Access-Control-Allow-Origin", value: "*" },
+            // { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            { key: 'Content-Type', value: 'application/json; charset=utf-8' }
           ]
         }
       ]
