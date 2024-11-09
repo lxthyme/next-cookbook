5afc47f268fa21a222bef6a2b109bff05f09abc1
lxthyme <lx314333@gmail.com>
2024-11-09 19:06:37 +0800

    feat: Tampermonkey script update

--- mockData/stylish/Tampermonkey/lx.douyu.com.js
+++ mockData/stylish/Tampermonkey/lx.douyu.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         lx.douyu.com
 // @namespace    http://tampermonkey.net/
-// @version      v1.0.3
+// @version      v1.0.4
 // @description  try to take over the world!
 // @author       lxthyme
 // @match			*://*.douyu.com/0*
@@ -14,6 +14,7 @@
 // @match			*://*.douyu.com/7*
 // @match			*://*.douyu.com/8*
 // @match			*://*.douyu.com/9*
+// @match			*://*.douyu.com/topic/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=douyu.com
 // @resource     customCSS https://raw.githubusercontent.com/lxthyme/next-cookbook/v14/mockData/stylish/douyu.com.css
 // @run-at       document-start
