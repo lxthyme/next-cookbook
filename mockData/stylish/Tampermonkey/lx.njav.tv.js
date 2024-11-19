adf9ddbb1197f3a289f6274990fae2d6aa6c65dd
lxthyme <lx314333@gmail.com>
2024-11-20 00:00:39 +0800

    feat: Tampermonkey script update

--- mockData/stylish/Tampermonkey/lx.njav.tv.js
+++ mockData/stylish/Tampermonkey/lx.njav.tv.js
@@ -4,7 +4,7 @@
 // @version      1.0.1
 // @description  try to take over the world!
 // @author       lxthyme
-// @match        https://njav.tv
+// @match        https://njav.tv/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nvav.tv
 // @resource     customCSS https://raw.githubusercontent.com/lxthyme/next-cookbook/refs/heads/v14/mockData/stylish/njav.tv.css
 // @run-at       document-end
