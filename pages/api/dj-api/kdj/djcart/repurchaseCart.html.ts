96498d9a0007bb12a80fcd994e45eeff7b5e67f6
lxthyme <lx314333@gmail.com>
2025-01-03 16:38:45 +0800

    feat: update

---
+++ pages/api/dj-api/kdj/djcart/repurchaseCart.html.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { mockData } from '@dj/hack.repurchaseCart.html'
+
+const API = (req: NextApiRequest, res: NextApiResponse) => {
+
+  const data = mockData
+
+  return new Promise(function (resolve) {
+    setTimeout(resolve.bind(null, resolve), 1000)
+  })
+    .then(() => {
+      res.status(200).json(data)
+    })
+}
+
+export default API
