b53b49021b60def340b3abf86284bb9cffc76b03
lxthyme <lx314333@gmail.com>
2025-08-27 18:26:15 +0800

    feat: update

--- pages/api/dj-api/kdj/coupon/receiveOne.html.ts
+++ pages/api/dj-api/kdj/coupon/receiveOne.html.ts
@@ -3,10 +3,18 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 const API = (req: NextApiRequest, res: NextApiResponse) => {
 
-  const data = mockData_failure
+  const { r_lot_number, rid } = req.headers
+  // const r_lot_number = header['r_lot_number']
+  console.log('-->r_lot_number: ', r_lot_number, rid);
+  let data = mockData_failure
                         // .t04111005
-                        // .t00990001
-                        .tSuccess
+                        // .t04111014
+                        // .t00100051
+                        .t00990001
+                        // .tSuccess
+  if(r_lot_number && rid) {
+    data = mockData_failure.tSuccess
+  }
 
   return new Promise(function (resolve) {
     setTimeout(resolve.bind(null, resolve), 1000)
